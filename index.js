dac5076494a76bddd746eb3603e022593ddfdfd9
atik94 <atikurrahmanatik94@gmail.com>
2022-11-26 13:59:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z8kj7tg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const categoriesOptionCollection = client.db("laptopResailMarket").collection("categoriesOption");
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const options = await categoriesOptionCollection.find(query).toArray();
+      res.send(options);
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
+
+app.get("/", async (req, res) => {
+  res.send("used products resail server is running");
+});
+
+app.listen(port, () => {
+  console.log(`used product resail running on port ${port}`);
+});
