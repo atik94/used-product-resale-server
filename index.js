35be24200fd78096c8b7102f70dd33224a605ea0
atik94 <atikurrahmanatik94@gmail.com>
2022-11-28 01:08:45 +0600

    my orders api commit

--- index.js
+++ index.js
@@ -29,6 +29,13 @@ async function run() {
     // });
 
     // Booking api
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const bookings = await bookingCollection.find(query).toArray();
+      res.send(bookings);
+    });
+
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       console.log(booking);
